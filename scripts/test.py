5f1cc51d47f3c52a4825a73be35038f80704ca94
bschuty <schutyteske@gmail.com>
2023-02-02 17:38:44 -0300

    progress threshold

--- scripts/test.py
+++ scripts/test.py
@@ -1,11 +1,12 @@
-import tools
+import hsitools
 import tifffile
 import numpy as np
 import PhasorLibrary
 import matplotlib.pyplot as plt
 
-im = tifffile.imread('/home/bruno/Documentos/Proyectos/hsi/data/01_Bead2A_405_16.lsm')
+im = tifffile.imread('/home/bruno/Documentos/Proyectos/hsipy/data/01_Bead2A_405_8.lsm')
 
-aux = tools.phasor(im)
-# PhasorLibrary.interactive(aux[0], aux[1], aux[2], 0.1, nbit=8)
+dc, g, s, _, _ = hsitools.phasor(np.asarray(im))
+# PhasorLibrary.interactive(aux[0], aux[1], aux[2], 0.2, nbit=8)
 
+x, y = hsitools.histogram_thresholding(dc, g, s, 2, 10)
