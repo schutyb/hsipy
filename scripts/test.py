d56fc34251cf3f3449e1b744c541bd319165ba65
Bruno Schüty <120675337+schutyb@users.noreply.github.com>
2023-03-23 17:40:40 -0300

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -2,35 +2,23 @@ import tifffile
 import numpy as np
 import hsitools
 import hsi_visualization
-import matplotlib.pyplot as plt
 
-imstack = tifffile.imread('/home/bruno/Documentos/Proyectos/hsipy/data/melpig_10.lsm')
+
+imstack = tifffile.imread('/home/bruno/Documentos/Proyectos/hsipy/data/paramecium/SP_paramesium_561_2laser_R1.lsm')
 dc, g, s, md, ph = hsitools.phasor(imstack)  # create instance of PhasorCalculator class
-
-try1 = False
-if try1:
-    hsi_visualization.interactive1(dc, g, s, 0.1, nbit=8, ncomp=3, filt=True, nfilt=3)
-
 hsi_stack = np.asarray(imstack)
 
-try2 = False
-if try2:
+
+try1 = True
+if try1:
     hsi_visualization.interactive1(dc, g, s, 0.1, nbit=8, ncomp=4, filt=True, nfilt=3, spectrums=True,
                                    hsi_stack=hsi_stack, lamd=np.arange(418, 718, 10))
 
-try3 = True
-if try3:
-    phint = np.array([90, 180])
-    mdint = np.array([0.4, 0.7])
+try2 = False
+# Test interactive2, show dc image and histogram in the first window and phasor
+# and pseudocolor image with the given phase and modulation range.
+if try2:
+    phint = np.array([40, 125])
+    mdint = np.array([0.65, 0.95])
     hsi_visualization.interactive2(dc, g, s, 8, ph, phint, modulation=md, mdint=mdint,
                                    histeq=True, filt=True, nfilt=2)
-
-a = False
-if a:
-    phint = np.array([90, 135])
-    mdint = np.array([0, 0.7])
-
-    rgb = hsitools.phase_modulation_image(ph, phint, md, mdint)
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(rgb)
